ae5491fb220a3791e98335c08cddf1a546cb92f8
Atharva Deshpande <desh.atharvad99@gmail.com>
2025-01-17 09:04:53 -0800

    Test v4

--- src/ArtistProfilePage.jsx
+++ src/ArtistProfilePage.jsx
@@ -6,7 +6,7 @@ import { FaTrash, FaChevronDown, FaChevronUp, FaPen } from 'react-icons/fa';
 import ArtworkEditModal from './ArtworkEditModal';
 import ArtistEventDialog from './ArtistEventDialog';
 import { useNavigate } from 'react-router-dom';
-import axios from "axios";
+//import axios from "axios";
 
 
 const ArtistProfilePage = () => {
